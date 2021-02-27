7b167db8e16e8665660ae3d3b3f0c94f62751490
osaldiran <osaldiran@gmail.com>
2021-02-28 01:21:16 +0300

    yeni_ders

---
+++ OOP2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Adi = "Ömer";
+            musteri1.Soyadi = "Saldıran";
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "2";
+            musteri1.TcNo = "1231231321";
+
+
+        }
+    }
+}
