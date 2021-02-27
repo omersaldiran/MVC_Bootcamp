44c2cfeb638d423028fad0014b26259f6882bd6b
osaldiran <osaldiran@gmail.com>
2021-02-27 19:07:38 +0300

    yeni

--- Tutorial1/Program.cs
+++ Tutorial1/Program.cs
@@ -10,8 +10,21 @@ namespace Tutorial1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Deneme");
-            Console.WriteLine("Merhaba");
+            Product product1 = new Product();
+
+            product1.Id = 1;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+            product1.CategoryId = 2;
+
+            Product product2 = new Product() { Id = 2, CategoryId = 3, ProductName = "Kalem", UnitPrice = 35, UnitsInStock = 5 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product2);
+            
+            Console.ReadKey();
+
         }
     }
 }
