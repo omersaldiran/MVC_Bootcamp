059ce34aca1bf6f06bb47a533ebbfe8286924893
osaldiran <osaldiran@gmail.com>
2021-03-07 20:04:06 +0300

    sql

---
+++ SQLQuery2.sql
@@ -0,0 +1,15 @@
+﻿--Select
+Select CompanyName Sirketadi,City Sehir from Customers
+
+Select * from Customers where City = 'London'
+
+Select * from Products where CategoryID = 1
+
+select * from Products order by UnitPrice desc
+
+select COUNT(*) from Products
+
+select CategoryID,COUNT(*) Adet from Products group by CategoryID having count(*)<10
+
+select * from Products inner join Categories
+on Products.CategoryID = Categories.CategoryID
